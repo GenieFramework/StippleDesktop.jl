a680de444d085c95d6d81b3ce597e191de53f2f3
PGimenez <gimenezfebrer@gmail.com>
2024-04-23 22:37:11 +0200

    removed unused vars

--- src/StippleDesktop.jl
+++ src/StippleDesktop.jl
@@ -8,9 +8,6 @@ function load(port::Int=8000)
     @info "Loading Genie app on port $port"
     Genie.loadapp()
     Genie.up(port,async=true)
-
-    npm = npm_cmd()
-    node = nodejs_cmd()
     js_dir = joinpath(dirname(dirname(pathof(StippleDesktop))), "assets", "js")
     run(`$npm --prefix $js_dir run start $port`)
 end
